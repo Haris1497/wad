731d1cedc3d1bfafb837cf823cd25b6eac83b222
Haris Ahmad <haris.ahmad.1497@gmail.com>
2018-11-27 16:39:49 +0500

    js

--- lectures/10/js/main.js
+++ lectures/10/js/main.js
@@ -1,116 +1,191 @@
-/*function  showAlertBox() {
-    alert("Display Message")
-}*/
+//============================================
+//-----------  STEP - 1 ----------------------
+//============================================
 
 
-/*
-function  displayName(name) {
-    alert("My Name is " + name);
+// var todos = [];
+// function add() {
+//     var task = document.getElementById("task").value;
+//     todos.push(task);
+//     document.getElementById('todos').innerText = todos;
+// }
+
+
+//============================================
+//-----------  STEP - 2 ----------------------
+//============================================
+
+
+/*var todos = [];
+function add() {
+    var task = document.getElementById('task').value;
+    todos.push(task);
+    document.getElementById('task').value = '';
+    show();
 }
-*/
-/* Parameter vs Argument */
-/*function  show Message() {
-    console.log("A message to the console");
-}*/
-
-
-/*function sum() {
-    var n1 = 5;
-    var n2 = 3;
-    var sum = n1 + n2;
-    alert("Sum is "+ sum);
-}*/
-
-
-/*function multiply(a,b) {
-    var result = a * b;
-    alert("Result: " + result);
-}*/
-
-/*
-expo();
-function expo() {
-    var a = 2;
-    //a = a**3;
-    a = Math.pow(a,5)
-    alert(typeof a);
-    alert(a);
-}
-*/
-//global Var
-var category = "electronics";
-//dt();
-function dt() {
-    //local variable
-    var length = 16;                               // Number
-    var lastName = "Johnson";                      // String
-    var a;                                          // Now x is undefined
-    var x = {firstName:"John", lastName:"Doe"};    // Object
-    var y = 16 + 4 + "Volvo";
-    var z = "Volvo" + 16 + 4;
-    var cars = ["Saab","Volvo","BMW"];
-    console.log("My fav Car is " + cars[2]);
-    var person = {
-        firstName : "John",
-        lastName  : "Doe",
-        age       : 50,
-        eyeColor  : "blue"
-    };
-
-    console.log(person.firstName + " is " + person.age + " years old.");
-
-    console.log(typeof "John");              // Returns "string"
-    console.log(typeof 3.14);                // Returns "number"
-    console.log(typeof true);               // Returns "boolean"
-    console.log(typeof false);              // Returns "boolean"
-    var j;
-    console.log(typeof j)                   // Returns "undefined" (if x has no value)
-    console.log(typeof {name:'john', age:34});  // Returns "Object"
-    console.log(typeof [1,2,3,4]);              // Returns "Object"
-    console.log(typeof null);                    // Returns "Object"
-    console.log(typeof function myFunc(){});        // Returns "function"
-}
-
-//dayOfTheWeek();
-function dayOfTheWeek() {
-    var day;
-    switch (new Date().getDay()) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
+function show() {
+    var ul = document.createElement('ul');
+    ul.classList.add("list-group");
+    for(var i=0; i<todos.length; i++){
+        var li = document.createElement('li');
+        li.innerHTML  = '<li>' + todos[i] + '</li>';
+        li.classList.add("list-group-item");
+        ul.appendChild(li);
     }
-    console.log("Today is " + day);
+    document.getElementById('todos').appendChild(ul);
+}*/
+
+
+//============================================
+//-----------  STEP - 3 ----------------------
+//============================================
+
+
+//
+// function getTodos() {
+//     var todos = [];
+//     var todos_str = localStorage.getItem('todo');
+//     if(todos_str !== null)
+//         todos = JSON.parse(todos_str);
+//     return todos;
+// }
+//
+// function add() {
+//     var task = document.getElementById('task').value;
+//     if(task.trim() == ''){
+//         document.getElementById('message').style.display = 'block';
+//         return false;
+//     } else {
+//         document.getElementById('message').style.display = 'none';
+//     }
+//     var todos = getTodos();
+//     todos.push({task: task});
+//     document.getElementById('task').value = '';
+//     localStorage.setItem('todo',JSON.stringify(todos));
+//     show();
+// }
+//
+//
+// function show() {
+//     document.getElementById('todos').innerText = '';
+//     var todos = getTodos();
+//     var ul = document.createElement('ul');
+//     ul.classList.add("list-group");
+//     for(var i=0; i<todos.length; i++){
+//         var li = document.createElement('li');
+//         li.innerHTML  = '<li>' + todos[i].task + '</li>' +
+//             '<button class="btn btn-danger">' +
+//             '<i class="fas fa-trash-o"></i> ' +
+//             '<span class="d-none d-sm-inline"> Delete </span> </button>';
+//         li.classList.add("list-group-item");
+//         ul.appendChild(li);
+//     }
+//     document.getElementById('todos').appendChild(ul);
+// }
+// show();
+//
+
+
+
+//============================================
+//-----------  STEP - 4 ----------------------
+//============================================
+
+
+function getTodos() {
+    var todos = [];
+    var todos_str = localStorage.getItem('todo');
+    if(todos_str !== null)
+        todos = JSON.parse(todos_str);
+    return todos;
 }
 
-//objectTesting();
-function objectTesting() {
-    var person = {
-        firstName: "John",
-        lastName : "Doe",
-        id       : 5566,
-        fullName : function() {
-            return this.firstName + " " + this.lastName;
-        }
-    };
-    console.log(person.firstName);
-    console.log(person['id']);
-    console.log(person.fullName());
+function add() {
+    var task = document.getElementById('task').value;
+    if(task.trim() == ''){
+        document.getElementById('message').style.display = 'block';
+        return false;
+    } else {
+        document.getElementById('message').style.display = 'none';
+    }
+    var todos = getTodos();
+    todos.push({task: task, isDone: false});
+    document.getElementById('task').value = '';
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = getTodos();
+    todos.splice(id,1);
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+    return false;
+}
+
+function show() {
+    document.getElementById('todos').innerText = '';
+    var todos = getTodos();
+    var ul = document.createElement('ul');
+    ul.classList.add("list-group");
+    for(var i=0; i<todos.length; i++){
+        var li = document.createElement('li');
+        li.innerHTML  = '<li contenteditable=\"true\" id=\"id' + i+1+ '\" onclick=\"StrikeThrough(this.id)\">' + todos[i].task + '</li>' +
+            '<button class="btn btn-danger" id="' + i + '">' +
+            '<i class="fa fa-trash-o"></i> ' +
+            '<span class="d-none d-sm-inline"> Delete </span> </button>'
+
+        li.classList.add("list-group-item");
+        if(todos[i].isDone)
+            li.classList.add("done");
+        ul.appendChild(li);
+    }
+    document.getElementById('todos').appendChild(ul);
+    var buttons = document.getElementsByClassName('btn-danger');
+    for(var i=0; i<buttons.length; i++){
+        buttons[i].addEventListener('click',remove);
+    }
+}
+
+function isDone(e) {
+    var todos = getTodos();
+    if(todos[e.target.id].isDone) {
+        e.target.classList.add('done');
+        todos[e.target.id].isDone = false;
+    }
+    else{
+        e.target.classList.remove('done');
+        todos[e.target.id].isDone = true;
+    }
+    localStorage.setItem('todo',JSON.stringify(todos));
+    show();
+}
+show();
+
+
+
+
+
+//============================================
+//-----------  STEP - 5 ----------------------
+//============================================
+
+/*
+CLASS ACTIVITY (HOME WORK) :
+1- Enable State of todos by clicking on the text completed, started etc
+HINT: use text-decoration:line-through; property of CSS
+2- Enable Editing todos in text field to update text
+*/
+
+
+
+function StrikeThrough(clickedID){
+
+    var li = document.getElementById(clickedID);
+    if(li.classList.contains("done"))
+        li.classList.remove("done");
+    else
+        li.classList.add("done")
 }
